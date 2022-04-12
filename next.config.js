199f5a081d08c4b832cbb491a165aa61fa37b560
cededuarte <70069755+cededuarte@users.noreply.github.com>
2022-04-13 04:14:05 +0800

    build project

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "cdn.freelogovectors.net",
+      "a0.muscache.com",
+      "images.contentstack.io",
+      "pbs.twimg.com",
+      "links.papareact.com",
+      "summitmedia-digital.com",
+      "google.com",
+      "theculturetrip.com"
+    ],
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoiY2VkZWR1YXJ0ZSIsImEiOiJjbDF3aXZrYjkwaDVvM2RxY2Fldnc2d3o1In0.LBMYDsKPRWXfoj0cN02fjQ'
+  }
+};
